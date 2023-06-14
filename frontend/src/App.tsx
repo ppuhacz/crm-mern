23bc4694c44d3c5fe8838be2114c06a2f93cd093
ppuhacz <ppuhacz@gmail.com>
2023-06-14 14:46:18 +0200

    Started working on a front-end of contacts list, fixed a invitation request bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.scss";
-import router from "./config/routes";
+import router from "./hooks/routes";
 
 function App() {
   return <div className='App'>{router}</div>;
